f8f7fea7c3322567f9aa6cb59fcd4f8a104e9d2c
Bernardo Cardoso <bernardo@xx.network>
2023-05-25 16:41:14 +0000

    Build script

--- build.sh
+++ build.sh
@@ -10,3 +10,11 @@ rm -f windows.syso
 rm -rf output/*
 cp -r electron/release/app/dist/renderer resources/app
 astilectron-bundler
+pushd output
+mv darwin-amd64/Proxxy.app Proxxy-mac-x64.app
+mv darwin-arm64/Proxxy.app Proxxy-mac-arm64.app
+mv linux-amd64/Proxxy Proxxy-linux-x64
+mv linux-arm64/Proxxy Proxxy-linux-arm64
+mv windows-amd64/Proxxy.exe Proxxy-win-x64.exe
+rm -r darwin-amd64 darwin-arm64 linux-amd64 linux-arm64 windows-amd64
+popd
