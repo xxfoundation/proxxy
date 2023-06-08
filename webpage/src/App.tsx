d515ecb811178602b03dde2a86ba9eb546877a17
Baltasar Aroso <baltasar@xx.network>
2023-06-08 16:30:36 -0500

    change logo design

--- webpage/src/App.tsx
+++ webpage/src/App.tsx
@@ -1,7 +1,9 @@
-import { Box, Container } from '@mui/material'
+import { Box, Container, Stack, Typography } from '@mui/material'
 import './App.css'
 import { Hello } from './Components/Hello'
-import icon from './assets/icon.svg'
+// import icon from './assets/icon.svg'
+import logo from './assets/whiteLogo.png'
+import madeBy from './assets/madeBy.png'
 
 function App() {
   return (
@@ -12,10 +14,16 @@ function App() {
           maxHeight: '1600px',
         }}
       >
-        <Box margin={2}>
-          <img width='200' alt='icon' src={icon} />
-        </Box>
+        <Stack mb={'1.5em'} direction={'row'} sx={{ justifyContent: 'center' }}>
+            <Box margin={1}>
+            <img width='50' alt='logo' src={logo} />
+          </Box>
+          <Typography variant='h2' sx={{ paddingBottom: '0.35em', alignSelf: 'center' }}>proxxy</Typography>
+        </Stack>
         <Hello />
+        <Box>
+          <img width='200' alt='madeBy' src={madeBy} />
+        </Box>
       </Container>
     </div>
   )
